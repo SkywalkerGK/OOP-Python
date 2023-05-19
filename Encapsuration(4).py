eaa4564eb58eae9bf9305fbd314a2e30448ade8b
Namcha <Lanend18@gmail.com>
2023-05-19 16:41:53 +0700

    Encapsuration

---
+++ Encapsuration(4).py
@@ -0,0 +1,23 @@
+class Employee: 
+
+    def __init__(self,name,salary):
+        print("Defalut Constructor")
+        #Public attribute
+        self._name = name
+        #Private attribute
+        self.__salary = salary
+
+    
+    def _showData(self):
+        #Pubric Method
+        print("ชื่อพนักงาน {}".format(self._name))
+        #Private Method
+        print("เงินเดือน {}".format(self.__salary))
+
+
+obj1 = Employee("Sorawit",23000)
+obj1._name = "Namcha"
+obj1.__salary = 50000
+obj1._showData()
+
+
